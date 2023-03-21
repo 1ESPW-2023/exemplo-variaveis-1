92d3bebee90425a055a460eb171225bd4c4af1a9
kaikyalvaro1708 <kaikialvaro57@gmail.com>
2023-03-21 20:22:52 -0300

    Formas de utilização do JS+ defer e precedencia

---
+++ rm98118/Js/script.js
@@ -0,0 +1,9 @@
+// capturando o elemento h1 e adicionando em uma constante
+
+const h1Element = document.getElementById("meu-título")
+
+// atrelando um evento ao elemento h1
+
+h1Element.addEventListener("click", function(){
+    alert("Agora com super-poderes!!!")
+})
