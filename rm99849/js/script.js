125b6cbc99fcb33649b3ae785e00a66c0d829410
Carlosqmenezes <RM99849@fiap.com.br>
2023-03-21 20:21:41 -0300

    formas de utilização do js

---
+++ rm99849/js/script.js
@@ -0,0 +1,7 @@
+//capturando o elemento h1 e adicionando em uma contante 
+const h1element = document.getElementById("meu-titulo")
+
+// atrelando um evento ao h1
+h1element.addEventListener("click",function(){
+    alert("não clique aí")
+})
