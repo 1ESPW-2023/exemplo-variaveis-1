ac23393f4e3a9fd0e2e7e174e65a738f35764158
AV-Rae <rm99011@fiap.com.br>
2023-03-21 20:29:57 -0300

    no comment

--- RM99011/js/script.js
+++ RM99011/js/script.js
@@ -5,5 +5,9 @@ const h1Element = document.getElementById("meu-titulo")
 
 //trelando um evento ao elemento h1
 h1Element.addEventListener("click",function () {
-    
+    alert("agora com super-poderes")
 })
+
+//vamos criar um array de frutas
+var frutas = ["maçã", "banana", "laranja", "melancia"]
+
