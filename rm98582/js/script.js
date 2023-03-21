aed0bd1b3c32a4412cdc516a989f13ed6defc9a1
Akiozin <rm98582@fiap.com.br>
2023-03-21 20:26:26 -0300

    Limpando o body e deixando só o doc do script.js

---
+++ rm98582/js/script.js
@@ -0,0 +1,9 @@
+
+//Capturando o elemento h1 e aficionando em uma 
+// constante
+const h1Element = document.getElementById("meu-titulo")
+
+//Atrelando um evento ao elemento H1
+h1Element.addEventListener("click",function(){
+    alert("teste")
+})
