815bf29d6c57dd29c82e449d166af6e8d8ebd3f8
daviguerra05 <rm551605@fiap.com.br>
2023-03-21 20:11:01 -0300

    commit inicial da aula 02 de JS

---
+++ rm551605/js/script.js
@@ -0,0 +1,7 @@
+//Capturando o elemento h1 e adicionando em uma constante
+const h1element = document.getElementById('meu-titulo')
+
+//Atrelando um evento ao elemento
+h1element.addEventListener("click",function(){
+    
+})
