adbd65699ac1096bbb0fbd1f98deade14a85e978
daviguerra05 <rm551605@fiap.com.br>
2023-03-21 20:59:29 -0300

    aulajs02

--- rm551605/js/script.js
+++ rm551605/js/script.js
@@ -1,7 +1,25 @@
 //Capturando o elemento h1 e adicionando em uma constante
-const h1element = document.getElementById('meu-titulo')
+const aqui = document.getElementById("aqui")
 
 //Atrelando um evento ao elemento
-h1element.addEventListener("click",function(){
-    
+aqui.addEventListener("click", function (){
+    alert("Agora com Super-poderes!!!")
+
 })
+
+var frutas = ["maçã", "banana", "laranja", "melancia"]
+
+console.log("Minhas frutas : " + frutas)
+console.log("UM ITEM: " + frutas[0])
+
+//Adicionar um valor ao final do array
+frutas.push("uva")
+console.log("Minhas frutas : " + frutas)
+console.table(frutas)
+
+for (let i = 0; i < frutas.length; i++) {
+    const element = frutas[i];
+    console.log("FRUTA SELECIONADA "+ element)
+}
+
+
