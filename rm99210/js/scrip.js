81d99fe31274d0e3e016dfd91fb7aab43ce5a1e8
lgustavobarre351 <rm99210@fiap.com.br>
2023-03-21 20:26:59 -0300

    Formas de utilização js

---
+++ rm99210/js/scrip.js
@@ -0,0 +1,10 @@
+
+//Capturando o elemento h1 e adicionando em uma 
+//constante
+const h1Element = document.getElementById("meu-titulo")
+
+//Atrelando um evento ao elemento h1
+h1Element.addEventListener("click", function () {
+    alert("Agora com super-poderes")
+  
+})
