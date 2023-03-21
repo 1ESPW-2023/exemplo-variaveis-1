6cdc204d258e82bae6b152d84ac5db09c6a761e1
LucasFeijoDev <LucasFeijoDev@gmail.com>
2023-03-21 20:52:17 -0300

    Novo comandos java Script

--- rm99727/js/script.js
+++ rm99727/js/script.js
@@ -5,3 +5,19 @@ const h1Element = document.getElementById('meu-titulo')
 h1Element.addEventListener('click', function () {
     alert('Agora com super-poderes!!!')
 })
+
+//Agora vamos ver um Array de frutas
+var frutas = ['maçã', 'banana', 'laranja', 'melancia']
+
+console.log("Minhas frutas: " + frutas)
+console.log("Um item apenas: " + frutas[0])
+
+//Adicionar um item ao final da array
+frutas.push('uva')
+console.log("Minhas frutas: " + frutas)
+console.table(frutas)
+
+for (let x = 0; x < frutas.length; x++) {
+    const element = frutas[x];
+    console.log('Fruta selecionada ' + element)    
+}
