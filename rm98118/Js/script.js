512af15d4d4b056402c7ec4b5c469e5bc6731f78
kaikyalvaro1708 <kaikialvaro57@gmail.com>
2023-03-21 20:54:05 -0300

    Array, leitura e declaração for

--- rm98118/Js/script.js
+++ rm98118/Js/script.js
@@ -3,7 +3,24 @@
 const h1Element = document.getElementById("meu-título")
 
 // atrelando um evento ao elemento h1
-
-h1Element.addEventListener("click", function(){
+/*
+h1Element.addEventListener("click", function (){
     alert("Agora com super-poderes!!!")
 })
+*/
+// agora vamos ver um array de frutas
+
+var frutas = ["maçã", "banana", "laranja", "melancia"]
+
+console.log("Minhas frutas:" + frutas)
+console.log("Um item:" + frutas[0])
+
+// adicionar um item ao final do array
+ frutas.push("uva")
+ console.log("Minhas uva:" + frutas)
+ console.table(frutas)
+
+for (let x =0; x < frutas.length; x++){
+    const element = frutas[x];
+    console.log("Frutas selecionada"+ element)
+}
