43f538b9cdc69ccd4348597a630d26d3012c6b84
BrnBastos <rm550416@fiap.com.br>
2023-03-21 20:55:21 -0300

    js aula

--- rm550416/js/script.js
+++ rm550416/js/script.js
@@ -5,3 +5,19 @@ const h1Element = document.getElementById("titulo")
 h1Element.addEventListener("click",function (){
     alert("agora com super-poderes")
 })
+
+//Agora vamos ver um array de fruta
+var frutas = ["maçã", "laranja", "banana", "melancia"]
+
+console.log("MINHAS FRUTAS: " + frutas)
+console.log("UM ITEM: " + frutas[0])
+
+//Adicionar um item ao final do array
+frutas.push("uva")
+console.log("MINHAS FRUTAS: " + frutas)
+console.table(frutas)
+
+for (let x = 0; x < frutas.length; x++){
+    const element = frutas[x];
+    console.log("FRUTA SELECIONADA " + element)
+}
