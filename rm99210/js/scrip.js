c1282f55bcd01927cb9b7cef663801cbfb96007e
lgustavobarre351 <rm99210@fiap.com.br>
2023-03-21 20:53:04 -0300

    Array.leitura e declaração + loop for.

--- rm99210/js/scrip.js
+++ rm99210/js/scrip.js
@@ -6,5 +6,20 @@ const h1Element = document.getElementById("meu-titulo")
 //Atrelando um evento ao elemento h1
 h1Element.addEventListener("click", function () {
     alert("Agora com super-poderes")
-  
-})
+  })
+
+//Agora vamos ver um array de frutas
+var Frutas = ["maça", "banana", "laranja", "melancia"]
+
+console.log("MINHAS FRUTAS : " + Frutas)
+console.log("UM ITEM : " + Frutas[0])
+ 
+//Adicionar um item ao final so array 
+Frutas.push("uva")
+console.log("MINHAS FRUTAS : " + Frutas)
+console.table(Frutas)
+
+for (let A = 0; A < Frutas.length; A++) {
+    const element = Frutas[A];
+    console.log("FRUTA SELECIONADA " + element)
+}
