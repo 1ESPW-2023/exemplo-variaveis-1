65fec731bf07950475fa9391c0d0d20e90d04d9e
matlucaas <rm99505@fiap.com.br>
2023-03-21 20:15:14 -0300

    criei um arquivo novo e mudanças no index

---
+++ rm99505/js/script.js
@@ -0,0 +1,9 @@
+
+//Capturando o elemento h1 e adicionando em uma
+//constante
+const h1Element = document.getElementById("meu-titulo")
+
+//Atrelando um evento ao elemento h1
+h1Element.addEventListener("click",function () {
+    alert("Agora com super-poderes!!!")
+})
