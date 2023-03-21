4b11d2470e6d3ac5b853fa91fa193a9ff79e83e2
Guimeb <rm551981@fiap.com.br>
2023-03-21 20:54:14 -0300

    Alteraçoes no js, exemplos de array no console

--- rm551981/html/js/script.js
+++ rm551981/html/js/script.js
@@ -6,3 +6,19 @@ const h1Element = document.getElementById("meu-titulo")
 h1Element.addEventListener("click",function () {
  alert("Agora com super-poderes!!!")
 })
+
+//Array de frutas
+var frutas = ["uva","banana","maça","melancia"]
+
+console.log("MINHAS FRUTAS : " + frutas)
+console.log("UM ITEM : " + frutas[0])
+
+//adicionar um item ao final do Array
+frutas.push("laranja")
+console.log("MINHAS FRUTRAS : " + frutas)
+console.table(frutas)
+
+for (let x = 0; x < frutas.length; x++) {
+    const element = frutas[x];
+    console.log("FRUTA SELECIONADA " + element)
+}
