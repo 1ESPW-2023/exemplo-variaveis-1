dd67dadd19962c49565a2988f587ea0a8e796c0f
AV-Rae <rm99011@fiap.com.br>
2023-03-21 20:19:10 -0300

    no comment

---
+++ RM99011/js/script.js
@@ -0,0 +1,9 @@
+
+//capturando o elemento h1 e adicionando em uma constante 
+//constante
+const h1Element = document.getElementById("meu-titulo")
+
+//trelando um evento ao elemento h1
+h1Element.addEventListener("click",function () {
+    
+})
