11a767378ccc47a9e8e66acd05692a234e027c62
andrelambert <rm99148@fiap.com.br>
2023-03-21 20:21:40 -0300

    formas de utilização do js (aula 21-03)

---
+++ rm99148/js/script.js
@@ -0,0 +1,7 @@
+// capturando o elemento h1 e adicionando em uma constante
+const h1Element = document.getElementById("meu-titulo")
+
+// atrelando um evento ao elemento h1
+h1Element.addEventListener("click" , function () { 
+    alert("Agora com super-poderes!!")
+})
