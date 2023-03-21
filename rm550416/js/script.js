f8903d85bfad3b49bf5a4a14fa03c8d76339d206
BrnBastos <rm550416@fiap.com.br>
2023-03-21 20:26:26 -0300

    js

---
+++ rm550416/js/script.js
@@ -0,0 +1,7 @@
+//Capturando o elemente h1 e adicionando em uma constante
+const h1Element = document.getElementById("titulo")
+
+//atrelando um evento ao elemento h1
+h1Element.addEventListener("click",function (){
+    alert("agora com super-poderes")
+})
