282c54e652dde921c5e98f02ee5fac5475a31a86
ruiasiqueira <rm98436@fiap.com.br>
2023-03-21 20:38:13 -0300

    Alteração

---
+++ rm98436/js/script.js
@@ -0,0 +1,17 @@
+
+//Campturando o elemento hq e adicionando em uma 
+//constante
+const h1Elemnt = document.getElementById('meu título')
+
+//Atrelando um evento ao elemento h1
+h1Element.addEventListener("click", function(){
+    alert("Agora comm super-poderes!!!")
+})
+
+//Agora vamos ver um Array de frutas
+var frutas = ["maçã","banana","laranja","melancia"]
+
+console.log("MINHAS FRUTAS : " + frutas)
+console.log("UM ITEM : " + frutas[0])
+
+
