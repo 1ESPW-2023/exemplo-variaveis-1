63b709f8ced95e1d590ffc92053b0709759e8b0b
matlucaas <rm99505@fiap.com.br>
2023-03-21 20:52:39 -0300

    cabo

--- rm99505/js/script.js
+++ rm99505/js/script.js
@@ -7,3 +7,20 @@ const h1Element = document.getElementById("meu-titulo")
 h1Element.addEventListener("click",function () {
     alert("Agora com super-poderes!!!")
 })
+
+//Agora vamos ver um Array de frutas
+var frutas = ["maçã", "banana", "laranja", "melancia"]
+
+console.log("MINHAS FRUTAS : " + frutas)
+console.log("UM ITEM : " + frutas[0])
+
+//Adicionar um item ao final do Array
+frutas.push("uva")
+console.log("MINHAS FRUTAS : " + frutas)
+console.table(frutas)
+
+for (let x = 0; x < frutas.length; x++) {
+    const element = frutas[x];
+    console.log("FRUTA SELECIONADA " + element)
+}
+
