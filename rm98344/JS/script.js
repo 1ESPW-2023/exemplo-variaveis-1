61708ee1e30dfde0f15fe1f5dd8c9ae46bc04f76
lucasrsilva <rm98344@fiap.com.br>
2023-03-21 20:22:56 -0300

    JS file

---
+++ rm98344/JS/script.js
@@ -0,0 +1,7 @@
+// Capturando o elemento h1 e adicionando em uma constante
+const h1Element = document.getElementById("MyTitle")
+
+//Atrelando um evento ao elemento h1
+h1Element.addEventListener("click", function(){
+    alert("NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO")
+})
