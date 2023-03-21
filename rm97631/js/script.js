ea019a906e5a78af93ae515bc034de0380892377
Fabr1c100 <rm97631@fiap.com.br>
2023-03-21 20:56:22 -0300

    Array

--- rm97631/js/script.js
+++ rm97631/js/script.js
@@ -1,9 +1,25 @@
-// Capturando o elemento h1 e adicionando em uma
-// constante
 
+//Capturando o elemento h1 e aficionando em uma 
+// constante
 const h1Element = document.getElementById("meu-titulo")
 
-//Atrelando um evento ao elemento h1
-h1Element.addEventListener("click", function(){
-    alert("Agora com super-poderes!!!")
+//Atrelando um evento ao elemento H1
+h1Element.addEventListener("click",function(){
+    alert("teste")
 })
+
+//Agora vamos ver um Array de frutas
+var frutas = ["maçã", "banana", "laranja", "melancia"]
+
+console.log("MINHAS FRUTAS : " + frutas)
+console.log("UM ITEM : " + frutas[0])
+
+//Adicionar um item ao final do Array
+frutas.push("uva")
+console.log("MINHAS FRUTAS : " + frutas)
+console.table(frutas)
+for(let x = 0; x < frutas.length; x++) {
+    const element = frutas[x];
+    console.log("Minha cestinha da feira :D " + element)
+}
+
