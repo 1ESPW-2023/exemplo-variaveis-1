d12f33469efe818a1b85ff7930626731d8598fc5
andrelambert <rm99148@fiap.com.br>
2023-03-21 20:53:58 -0300

    aula 23-02 parte 2

--- rm99148/js/script.js
+++ rm99148/js/script.js
@@ -5,3 +5,19 @@ const h1Element = document.getElementById("meu-titulo")
 h1Element.addEventListener("click" , function () { 
     alert("Agora com super-poderes!!")
 })
+
+//agora vamos ver um array de frutas
+var frutas = ["maçã" , "banana" , "laranja" , "melancia"]
+
+console.log("MINHAS FRUTAS : " + frutas)
+console.log ("UM ITEM : " + frutas[0])
+
+// adicionar um item ao final do array
+frutas.push("uva")
+console.log("MINHAS FRUTAS : " + frutas)
+console.table(frutas)
+
+for (let x = 0; x < frutas.length; x++) {
+    const element = frutas[x];
+    console.log("FRUTA SELECIONADA " + element)    
+}
