89695d60d52044d57874313682a02e897cec5ca3
FreitasRj1 <rm550187@fiap.com.br>
2023-03-21 20:23:19 -0300

    Formas de utilização Js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//Capturando o elemento h1 e o adcionando em uma 
+// Constante
+const h1Element = document.getElementByld("meu-titulo")
+
+//Atrelando um evento ao h1
+h1Element.addEventListener ("click",function (){
+    alert("agora com super-poderes!!!")
+}
+)
