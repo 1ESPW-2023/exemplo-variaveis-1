4dc8d9793b053ba2e3608a8de5fdfc6131585036
LucasFeijoDev <LucasFeijoDev@gmail.com>
2023-03-21 20:21:50 -0300

    Formas de utilização do JS + prefer e precedencia.

---
+++ rm99727/js/script.js
@@ -0,0 +1,7 @@
+//Capturando o elemento h1 e adicionando em uma
+const h1Element = document.getElementById('meu-titulo')
+
+//Atrelando um evento ao elemento h1
+h1Element.addEventListener('click', function () {
+    alert('Agora com super-poderes!!!')
+})
