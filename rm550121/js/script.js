3a66f2eec1ea128b9a6d273761be24b682722520
Gabriel-Machado-Belardino <rm550121@fiap.com.br>
2023-03-21 21:03:48 -0300

    Progress of 21/03 (Linking JS and using Arrays)

---
+++ rm550121/js/script.js
@@ -0,0 +1,16 @@
+
+const titulo = document.getElementById("titulo-3")
+
+titulo.addEventListener("click",function (){
+    alert("Estou te avisando irmão... fica esperto!")
+})
+
+var frutas = ["Maçã","Banana", "Laranja", "Melancia"]
+
+frutas.push("uva")
+
+console.table(frutas)
+
+
+console.log("MINHAS FRUTAS : " + frutas)
+
