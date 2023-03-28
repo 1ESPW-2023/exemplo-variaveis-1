b1fa42b6f131fd23693c1a852069c7651c218132
LucasFeijoDev <lucasfeijodev@gmail.com>
2023-03-28 20:55:57 -0300

    atualizações da aula 28/03

---
+++ rm99727/js/28_03.js
@@ -0,0 +1,29 @@
+var nome = 'Lucas Feijó'
+var idade = 18
+var email = 'lucasfeijodev@gmail.com'
+
+console.log(nome + ', ' + idade  + ', ' + email)
+
+var idade = 30 
+
+console.log(idade)
+
+var x = 10
+var y = 5
+
+console.log(x + y)
+
+var frase = 'Estou fazendo a aula de JavaScript'
+
+console.log(frase)
+
+var verdadeiro = true
+
+console.log(typeof(verdadeiro))
+console.log(verdadeiro)
+
+console.log(x * y)
+
+var vazio = null
+
+console.log(vazio)
