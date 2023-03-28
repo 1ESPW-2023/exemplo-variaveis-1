b9a9283528e74e0e1688f4edeac34d5c323589b3
alevaiano <rm551497@fiap.com.br>
2023-03-28 20:07:40 -0300

    commit aula 28 março

---
+++ rm551497/28_março.js
@@ -0,0 +1,57 @@
+// se eu quiser abrir em html devo fazer o link no index.html (em um file que tenha html)
+//porém devo salvar onde fiz o link e o arquivo que copiei tbm pra poder dar run
+
+//Agora vamos ver um Array de frutas
+//var frutas = ["maçã", "banana","laranja","melancia"]
+
+//console.log("MINHAS FRUTAS : " + frutas)
+//console.log("UM ITEM : " + frutas[0])
+
+//Adicionar um item ao final do Array
+//frutas.push("uva")
+//console.log("MINHAS FRUTAS : " + frutas)
+//console.table(frutas)
+
+//for (let x = 0; x < frutas.length; x++) {
+//    const element = frutas[x];
+//    console.log("FRUTA SELECIONADA " + element)
+//}
+
+
+//Realizando a leitura com FOREACH
+//let n1 = [10,20,30,40,50,60]
+
+    // n1.forEach(numero =>{
+    //     console.log("Apresentando o conteúdo do array : " + numero)
+    // })
+    
+    //for off 
+    //for (let numeros of n1) {
+    //     console.log("Apresentando o conteúdo do array : " + numeros)
+    // }
+
+    //for in: posição
+    // for (let posicao in n1) {
+    //    console.log("Apresentando o conteúdo do array : " + n1[posicao])
+    //    console.log(`Apresentando o conteúdo do array : ${n1[posicao]}`) //interpolação
+    //}
+
+
+
+
+
+//  
+    let n1 = [10,20,30,40,50,60]
+    let n2 = [70,80,90,100,110,120,130,140]
+
+//CONCATENANDO ARRAYS
+
+// forma normal:
+//let n3 = [n1,n2]
+//console.log(`Conteúdo do novo array: ${n3}`)
+
+//forma mais simples: spread
+//let n3 = [...n1,n2]
+//    console.log(`Conteúdo do novo array: ${n3}`)
+
+
