984dc890538daf53f694a2d3c3de671e42f1790c
daviguerr05 <rm551605@fiap.com.br>
2023-03-28 20:27:33 -0300

    commit das cores

--- rm551605/js/script.js
+++ rm551605/js/script.js
@@ -1,29 +1,30 @@
-//Capturando o elemento h1 e adicionando em uma constante
-//     const aqui = document.getElementById("aqui")
 
-// //Atrelando um evento ao elemento
-//  aqui.addEventListener("click", function (){
-//          alert("Agora com Super-poderes!!!")
+//Capturando o elemento h1 e adicionando em uma
+// constante
+// const h1Element = document.getElementById("meu-titulo")
 
-//  })
+// //Atrelando um evento ao elemento h1
+// h1Element.addEventListener("click",function () {
+//     alert("Agora com super-poderes!!!")
+// })
 
-// var frutas = ["maçã", "banana", "laranja", "melancia"]
+//Agora vamos ver um Array de frutas
+// var frutas = ["maçã", "banana","laranja","melancia"]
 
-// console.log("Minhas frutas : " + frutas)
-// console.log("UM ITEM: " + frutas[0])
+// console.log("MINHAS FRUTAS : " + frutas)
+// console.log("UM ITEM : " + frutas[0])
 
-//Adicionar um valor ao final do array
+// //Adicionar um item ao final do Array
 // frutas.push("uva")
-// console.log("Minhas frutas : " + frutas)
+// console.log("MINHAS FRUTAS : " + frutas)
 // console.table(frutas)
 
-// for (let i = 0; i < frutas.length; i++) {
-//     const element = frutas[i];
-//     console.log("FRUTA SELECIONADA "+ element)
+// for (let x = 0; x < frutas.length; x++) {
+//     const element = frutas[x];
+//     console.log("FRUTA SELECIONADA " + element)
 // }
 
-//realizando a leitura FOREACH
-
+//Realizando a leitura com FOREACH
 // let n1 = [10,20,30,40,50,60]
 
     // n1.forEach(numero =>{
@@ -38,11 +39,22 @@
     //     console.log(`Apresentando o conteúdo do array : ${n1[posicao]}`)
     // }
 
-    
-    let n1 = [10,20,30,40,50,60]
-    let n2 = [70,80,90,100,110,120,130,140]
+    // let n1 = [10,20,30,40,50,60]
+    // let n2 = [70,80,90,100,110,120,130,140]
 
-    //CONCATENANDO ARRAYS
-    // let n3 = [n1,n2]
-    let n3 = [...n1,n2]
-    console.log(`Conteúdo do novo array: ${n3}`)
+    // //CONCATENANDO ARRAYS
+    // // let n3 = [n1,n2]
+    // let n3 = [...n1,n2]
+    // console.log(`Conteúdo do novo array: ${n3}`)
+
+    // const listaDeElementosHtml = document.getElementsByTagName("li")
+    // const listaDeElementosHtmlArray = [...listaDeElementosHtml]
+    // let indice = 0
+    // listaDeElementosHtmlArray.forEach(elementoLi =>{
+    //     ++indice
+    //     elementoLi.innerHTML = (`Novo Item - ${indice}`)
+    //     elementoLi.style.backgroundColor = "#B81365";
+    //     console.log(elementoLi.textContent)
+    // })
+
+    
