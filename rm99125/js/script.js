f807760d98fdde6064ce2c7d5c4332b472708609
ViniciusBuzato <rm99125@fiap.com.br>
2023-03-28 20:06:38 -0300

    For, off, in, interpolação e iniciando spread

--- rm99125/js/script.js
+++ rm99125/js/script.js
@@ -1,26 +1,32 @@
 
-//Capturando o elemento h1 e adcionando em uma 
-// constante
-const h1Element = document.getElementById("meu-titulo")
+// //Capturando o elemento h1 e adcionando em uma 
+// // constante
+// const h1Element = document.getElementById("meu-titulo")
 
-//Atrelando um evento ao elemento h1
-h1Element.addEventListener("click", function(){
-    alert("Agora com super-poderes!!!")
-})
+// //Atrelando um evento ao elemento h1
+// h1Element.addEventListener("click", function(){
+//     alert("Agora com super-poderes!!!")
+// })
 
-// Agora vamos ver um array de frutas
-var frutas = ["maçã", "banana", "laranja", "melancia",]
+// // Agora vamos ver um array de frutas
+// var frutas = ["maçã", "banana", "laranja", "melancia",]
 
-console.log("MINHAS FRUTAS : " + frutas)
-console.log("UM ITEM : " + frutas[0])
+// console.log("MINHAS FRUTAS : " + frutas)
+// console.log("UM ITEM : " + frutas[0])
 
-//Adicionar um item ao final do Array
-frutas.push("uva")
-console.log("MINHAS FRUTAS : " + frutas)
-console.table(frutas)
+// //Adicionar um item ao final do Array
+// frutas.push("uva")
+// console.log("MINHAS FRUTAS : " + frutas)
+// console.table(frutas)
 
-for(let x = 0; x < frutas.length; x++) {
-    const element = frutas[x];
-    console.log("Fruta selecionada : " + element)
-}
+// for(let x = 0; x < frutas.length; x++) {
+//     const element = frutas[x];
+//     console.log("Fruta selecionada : " + element)
+// }
 
+// Realizando a leitura com FOREACH
+let n1 = [10,20,30,40,50,60]
+
+    n1.forEach(numero =>{
+        console.log("Apresentando o conteúdo do array : " + numero)
+    })
