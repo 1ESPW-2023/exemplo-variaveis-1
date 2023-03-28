8491e9cd41fb815da7cb47733f4bffa6ff5f3dfe
kaikyalvaro1708 <kaikialvaro57@gmail.com>
2023-03-28 19:46:40 -0300

    apresentando o array

--- rm98118/Js/script.js
+++ rm98118/Js/script.js
@@ -10,17 +10,27 @@ h1Element.addEventListener("click", function (){
 */
 // agora vamos ver um array de frutas
 
-var frutas = ["maçã", "banana", "laranja", "melancia"]
+// var frutas = ["maçã", "banana", "laranja", "melancia"]
 
-console.log("Minhas frutas:" + frutas)
-console.log("Um item:" + frutas[0])
+// console.log("Minhas frutas:" + frutas)
+// console.log("Um item:" + frutas[0])
 
-// adicionar um item ao final do array
- frutas.push("uva")
- console.log("Minhas uva:" + frutas)
- console.table(frutas)
+// // adicionar um item ao final do array
+//  frutas.push("uva")
+//  console.log("Minhas uva:" + frutas)
+//  console.table(frutas)
 
-for (let x =0; x < frutas.length; x++){
-    const element = frutas[x];
-    console.log("Frutas selecionada"+ element)
-}
+// for (let x =0; x < frutas.length; x++){
+//     const element = frutas[x];
+//     console.log("Frutas selecionada"+ element)
+// }
+
+
+// realizando a leitura com FOREACH
+
+let n1 =[10,20,30.40,50,60]
+
+     n1.forEach(numero =>{
+        console.log("Apresendo o conteúdo do array: " + numero)
+     }
+    
