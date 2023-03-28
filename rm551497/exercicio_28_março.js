96451c43453b32608d4b4edab05f6d1ba3537c16
alevaiano <rm551497@fiap.com.br>
2023-03-28 20:57:28 -0300

    exercício 28 março

---
+++ rm551497/exercicio_28_março.js
@@ -0,0 +1,34 @@
+// declare uma var chamada nome e atribua seu nove
+// declare uma var chamada idade e atribua sua idade
+// declare uma var chamada email e atribua seu email
+// imprima as variaveis no console "nome","idade" e "email", separando-as com um vérgula
+var nome = "Alessandra"
+var idade = 29
+var email = "alezvaiano@gmail.com"
+
+console.log(nome + ',' + idade + ',' + email)
+// altere o valor da var idade para 30 e imprima no console
+
+var idade = 30
+console.log(idade)
+
+// declare 2 var e atribua valores numéricos a elas. Em seguida apresente o resultado da soma desses valores no cosole;
+var b = 12
+var a = 45
+console.log(b + a)
+
+//declare uma var e atribua uma string a ela. em seguida apresente essa string  no console
+var frase = "oi. bom dia"
+console.log(frase)
+
+//declare uma var e atribua um  valor boleano (true or false) a ela. apresente no console
+var estudante = true
+console.log(estudante)
+
+//declare duas var e atribua valores numericos a elas. apresente a multiplicação no console
+var d = 40
+var c = 50
+console.log(d*c)
+
+//declare uma var e atribua o valor null a ela. apresente no console
+var x = null
