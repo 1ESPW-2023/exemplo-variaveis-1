907a790bb769e3b16d1556e3b9c1da01383bc926
lgustavobarre351 <rm99210@fiap.com.br>
2023-03-28 20:27:35 -0300

    eventos com spread em htmlcollections p array

--- rm99210/js/scrip.js
+++ rm99210/js/scrip.js
@@ -1,25 +1,45 @@
 
 //Capturando o elemento h1 e adicionando em uma 
 //constante
-const h1Element = document.getElementById("meu-titulo")
+// const h1Element = document.getElementById("meu-titulo")
 
-//Atrelando um evento ao elemento h1
-h1Element.addEventListener("click", function () {
-    alert("Agora com super-poderes")
-  })
+// //Atrelando um evento ao elemento h1
+// h1Element.addEventListener("click", function () {
+//     alert("Agora com super-poderes")
+//   })
 
-//Agora vamos ver um array de frutas
-var Frutas = ["maça", "banana", "laranja", "melancia"]
+// //Agora vamos ver um array de frutas
+// var Frutas = ["maça", "banana", "laranja", "melancia"]
 
-console.log("MINHAS FRUTAS : " + Frutas)
-console.log("UM ITEM : " + Frutas[0])
+// console.log("MINHAS FRUTAS : " + Frutas)
+// console.log("UM ITEM : " + Frutas[0])
  
-//Adicionar um item ao final so array 
-Frutas.push("uva")
-console.log("MINHAS FRUTAS : " + Frutas)
-console.table(Frutas)
+// //Adicionar um item ao final so array 
+// Frutas.push("uva")
+// console.log("MINHAS FRUTAS : " + Frutas)
+// console.table(Frutas)
 
-for (let A = 0; A < Frutas.length; A++) {
-    const element = Frutas[A];
-    console.log("FRUTA SELECIONADA " + element)
-}
+// for (let A = 0; A < Frutas.length; A++) {
+//     const element = Frutas[A];
+//     console.log("FRUTA SELECIONADA " + element)
+// }
+
+const listaDeElementosHtml = document.getElementsByTagName("li")
+const listaDeElementosHtmlArray = [...listaDeElementosHtml]
+let indice = 0
+
+listaDeElementosHtmlArray.forEach(elementoLi =>{
+
+elementoLi.style.backgroundColor = "red"
+
+   ++indice
+  elementoLi.innerHTML = (`Novo item - ${indice}`)
+  console.log(elementoLi.textContent)
+}) 
+
+
+
+
+
+
+
