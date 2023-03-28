c624ddffd7b0a4373d5c33a5e9ff63cf684e48a2
ruiasiquera <rm98436@fiap.com.br>
2023-03-28 20:07:08 -0300

    For, off, interpolação e iniciando spread.

--- rm98436/js/script.js
+++ rm98436/js/script.js
@@ -25,11 +25,30 @@
 // //}
 
 //Realizando a leitura com FOREACH
-let n1 = [10,20,30,40,50,60]
+// let n1 = [10,20,30,40,50,60]
 
-    n1.forEach(numero =>{
-        console.log("Apresentando o conteúdo do array : " + numero)
-    })
+    // n1.forEach(numero =>{
+    //     console.log("Apresentando o conteúdo do array : " + numero)
+    // })
+    // for (let numeros of n1) {
+    //     console.log("Apresentando o conteúdo do array : " + numeros)
+    // }
+
+    // for (let posicao in n1) {
+    //     // console.log("Apresentando o conteúdo do array : " + n1[posicao])
+    //     console.log(`Apresentando o conteúdo do array : ${n1[posicao]}`)
+    // }
+
+    let n1 = [10,20,30,40,50,60]
+    let n2 = [70,80,90,100,110,120,130,140]
+
+    //CONCATENANDO ARRAYS
+    // let n3 = [n1,n2]
+    let n3 = [...n1,n2]
+    console.log(`Conteúdo do novo array: ${n3}`)
+
+
+
 
 
 
