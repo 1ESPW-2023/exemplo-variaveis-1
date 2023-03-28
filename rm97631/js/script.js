2f53ebceb4b14a8ae97303147ebe8fb0781f4bb6
F@br1c10 <rm97631@fiap.com.br>
2023-03-28 19:45:59 -0300

    Apresentação do ForEach

--- rm97631/js/script.js
+++ rm97631/js/script.js
@@ -1,25 +1,32 @@
 
 //Capturando o elemento h1 e aficionando em uma 
 // constante
-const h1Element = document.getElementById("meu-titulo")
+// const h1Element = document.getElementById("meu-titulo")
 
-//Atrelando um evento ao elemento H1
-h1Element.addEventListener("click",function(){
-    alert("teste")
-})
+// //Atrelando um evento ao elemento H1
+// h1Element.addEventListener("click",function(){
+//     alert("teste")
+// })
 
 //Agora vamos ver um Array de frutas
-var frutas = ["maçã", "banana", "laranja", "melancia"]
+// var frutas = ["maçã", "banana", "laranja", "melancia"]
 
-console.log("MINHAS FRUTAS : " + frutas)
-console.log("UM ITEM : " + frutas[0])
+// console.log("MINHAS FRUTAS : " + frutas)
+// console.log("UM ITEM : " + frutas[0])
 
-//Adicionar um item ao final do Array
-frutas.push("uva")
-console.log("MINHAS FRUTAS : " + frutas)
-console.table(frutas)
-for(let x = 0; x < frutas.length; x++) {
-    const element = frutas[x];
-    console.log("Minha cestinha da feira :D " + element)
-}
+// //Adicionar um item ao final do Array
+// frutas.push("uva")
+// console.log("MINHAS FRUTAS : " + frutas)
+// console.table(frutas)
 
+// for(let x = 0; x < frutas.length; x++) {
+//     const element = frutas[x];
+//     console.log("Minha cestinha da feira" + element)
+// }
+
+//Realizando a leitura com FOREACH
+let n1 = [10,20,30,40,50,60]
+
+    n1.forEach(numero => {
+        console.log('Apresentando o conteúdo do array: '+numero)
+    })
