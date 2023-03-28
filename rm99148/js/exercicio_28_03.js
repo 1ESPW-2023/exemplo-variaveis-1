42de70c1e770ac0769c80f0a53b67f6c1ac29a92
andrelambert <rm99148@fiap.com.br>
2023-03-28 20:56:22 -0300

    aula dia 28-03

---
+++ rm99148/js/exercicio_28_03.js
@@ -0,0 +1,23 @@
+let nome = "André Lambert"
+var idade = 25
+let email = "andreflambert@gmail.com"
+
+console.log(nome + "," + idade + "," + email + ",")
+
+var idade = 30
+console.log(idade)
+
+var x = 10 
+var y = 15 
+console.log(x + y)
+
+var frase = "Estou fazendo aula de JavaScript"
+console.log(frase)
+
+var verdadeiro = true
+console.log(verdadeiro)
+
+console.log(x * y)
+var vazio = null
+console.log(vazio)
+
--- rm99148/js/script.js
+++ rm99148/js/script.js
@@ -6,18 +6,51 @@ h1Element.addEventListener("click" , function () {
     alert("Agora com super-poderes!!")
 })
 
-//agora vamos ver um array de frutas
-var frutas = ["maçã" , "banana" , "laranja" , "melancia"]
+// //agora vamos ver um array de frutas
+// var frutas = ["maçã" , "banana" , "laranja" , "melancia"]
 
-console.log("MINHAS FRUTAS : " + frutas)
-console.log ("UM ITEM : " + frutas[0])
+// console.log("MINHAS FRUTAS : " + frutas)
+// console.log ("UM ITEM : " + frutas[0])
 
-// adicionar um item ao final do array
-frutas.push("uva")
-console.log("MINHAS FRUTAS : " + frutas)
-console.table(frutas)
+// // adicionar um item ao final do array
+// frutas.push("uva")
+// console.log("MINHAS FRUTAS : " + frutas)
+// console.table(frutas)
 
-for (let x = 0; x < frutas.length; x++) {
-    const element = frutas[x];
-    console.log("FRUTA SELECIONADA " + element)    
-}
+// for (let x = 0; x < frutas.length; x++) {
+//     const element = frutas[x];
+//     console.log("FRUTA SELECIONADA " + element)    
+// }
+
+//Realizando a leitura com FOREACH
+// let n1 = [10,20,30,40,50,60]
+
+    // n1.forEach(numero =>{
+    //     console.log("Apresentando o conteúdo do array : " + numero)
+    // })
+    // for (let numeros of n1) {
+    //     console.log("Apresentando o conteúdo do array : " + numeros)
+    // }
+
+    // for (let posicao in n1) {
+    //     // console.log("Apresentando o conteúdo do array : " + n1[posicao])
+    //     console.log(`Apresentando o conteúdo do array : ${n1[posicao]}`)
+    // }
+
+    // let n1 = [10,20,30,40,50,60]
+    // let n2 = [70,80,90,100,110,120,130,140]
+
+    // //CONCATENANDO ARRAYS
+    // // let n3 = [n1,n2]
+    // let n3 = [...n1,n2]
+    // console.log(`Conteúdo do novo array: ${n3}`)
+
+    // const listaDeElementosHtml = document.getElementsByTagName("li")
+    // const listaDeElementosHtmlArray = [...listaDeElementosHtml]
+    // let indice = 0
+    // listaDeElementosHtmlArray.forEach(elementoLi =>{
+    //     ++indice
+    //     elementoLi.innerHTML = (`Novo Item - ${indice}`)
+    //     elementoLi.style.backgroundColor = "#f000f0";
+    //     console.log(elementoLi.textContent)
+    // })
