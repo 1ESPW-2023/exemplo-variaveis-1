4ad5efad73b1a2a5346cb97f155923be50542de5
Blue260910 <victorhugoaranda2609@gmail.com>
2023-03-28 19:28:39 -0300

    Adequação para andamento da aula

---
+++ rm99667/JS/script.js
@@ -0,0 +1,24 @@
+//Capturando o elemento h1 e adicionando em uma 
+//constante
+const h1Element = document.getElementById("Titulo")
+
+//Atrelando um evento ao elemento h1
+h1Element.addEventListener("click", function () {
+    alert("Agora com super-poderes")
+  })
+
+//Agora vamos ver um array de frutas
+var Frutas = ["maça", "banana", "laranja", "melancia"]
+
+console.log("MINHAS FRUTAS : " + Frutas)
+console.log("UM ITEM : " + Frutas[0])
+ 
+//Adicionar um item ao final so array 
+Frutas.push("uva")
+console.log("MINHAS FRUTAS : " + Frutas)
+console.table(Frutas)
+
+for (let A = 0; A < Frutas.length; A++) {
+    const element = Frutas[A];
+    console.log("FRUTA SELECIONADA " + element)
+}
