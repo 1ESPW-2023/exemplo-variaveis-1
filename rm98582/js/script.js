9ec2d5d4a03a59eda84d8d1ebbd822454e2b6ede
Akiozin <rm98582@fiap.com.br>
2023-04-04 20:02:04 -0300

    Exemplo de map

--- rm98582/js/script.js
+++ rm98582/js/script.js
@@ -1,26 +1,67 @@
+// const h1Element = document.getElementById("meu-titulo")
 
-//Capturando o elemento h1 e aficionando em uma 
-// constante
-const h1Element = document.getElementById("meu-titulo")
+// h1Element.addEventListener("click",function () {
+//     alert("Baianinho das casas bainha é una farsita!!!!")
+// })
 
-//Atrelando um evento ao elemento H1
-h1Element.addEventListener("click",function(){
-    alert("teste")
+// var frutas = ["maçã", "banana", "laranja", "melancia"]
+
+//console.log("MINHAS FRUTAS : " + frutas)
+//console.log("UM ITEM : " + frutas[0])
+
+//frutas.push("uva")
+//console.log("MINHAS FRUTAS : " + frutas)
+//console.table(frutas)
+
+//for (let x = 0; x < frutas.length; x++) {
+//    const element = frutas[x];
+//   console.log("FRUTA SELECIONADA " + element)
+//}
+
+//n1.forEach(numero =>{
+//    console.log("Apresentando o conteudo do array : " + numero)
+//})
+//for (let numeros of n1) {
+//    console.log("Apresentando o conteudo do array : " + numeros)
+//}
+
+    // for (let position in n1) {
+        //console.log("Apresentando o conteudo do array : " + n1[posicao])
+        // console.log(`Apresentando o conteudo do array : ${n1[posicao]}`)
+    // }
+
+    // let n1 = [10,20,30,40,50,60]
+    // let n2 = [70,80,90,100,110,120,130,140]
+
+    // CONCATENANDO ARRAYS
+    // let n3 = [n1,n2]
+    // let n3 = [...n1]
+    // console.log(`Conteudo do novo array: ${n3}`)
+
+    // const listaDeElementosHtml = document.getElementsByTagName("li")
+    // const listaDeElementosHtmlArray = [...listaDeElementosHtml]
+    // let indice = 0
+    // listaDeElementosHtmlArray.forEach(elementoli =>{
+        // ++indice
+        // elementoli.innerHTML = (`Novo Item - ${indice}`)
+        // elementoli.style.backgroundColor = "#ff0000";
+        // console.log(elementoli.textContent)
+    // })
+     
+
+// var nome = "Matheus"
+// var idade = 18
+// var email = "rm550352@fiap.com.br"
+
+// console.log(nome, idade, email)
+
+const el = [... document.getElementsByTagName("li")]
+
+//Utilização do MAP
+el.map((e,i)=>{
+
+    e.innerText = "NOVO ITEM"
+
 })
-
-//Agora vamos ver um Array de frutas
-var frutas = ["maçã", "banana", "laranja", "melancia"]
-
-console.log("MINHAS FRUTAS : " + frutas)
-console.log("UM ITEM : " + frutas[0])
-
-//Adicionar um item ao final do Array
-frutas.push("uva")
-console.log("MINHAS FRUTAS : " + frutas)
-console.table(frutas)
-for(let x = 0; x < frutas.length; x++) {
-    const element = frutas[x];
-    console.log("Minha cestinha da feira :D " + element)
-}
 
 
