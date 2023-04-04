f28162ea85649db644754d450c61f2cf6ef554c3
alevaiano <rm551497@fiap.com.br>
2023-04-04 20:49:20 -0300

    aula 4 abril

---
+++ rm551497/4_abril.js
@@ -0,0 +1,59 @@
+//tag dentro da tag - innerHTML: "Item-1"
+
+// const el = [...document.getElementsByTagName("li")]
+
+// //Utilização do MAP
+// el.map((e,i)=>{
+//     e.innerText = "NOVO ITEM"
+// })
+
+    //const el = [...document.getElementsByTagName("li")]
+
+        //Loop While:
+
+        //let key = true
+        //while (key) {
+        //}
+
+        //Loop While:
+
+        // let key = true
+        // let indice = 0
+
+        // console.log(el.length)
+
+        // while (key) {
+        //     console.log(`Valor do elemento li ${el[indice].innerText}`);
+        //     key = (indice == el.length) ? false : true;
+        //     indice++;
+        // }
+
+     //   switch (key) {
+     //       case value:
+        
+     //           break;
+
+     //       default:
+     //           break;
+     //   }
+
+     
+        //let diaSemana = "DOM";
+
+            //switch (diaSemana) {
+            //    case "SEG": 
+            //        console.log("Hoje é segunda-feira!")
+            //        break;
+            //    case "TER": 
+            //        console.log("Hoje é terça-feira!")
+            //        break;
+            //    case "QUA": 
+            //        console.log("Hoje é terça-feira!")
+            //        break;
+            //    default:
+            //        console.log("Hoje é ...")
+            //        break;
+            //}
+
+
+
