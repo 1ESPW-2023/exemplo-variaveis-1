24ec53c7b5adeae986d367991dc5658b3c67521d
Blue260910 <rm99667@fiap.com.br>
2023-04-04 20:04:32 -0300

    el.map

--- rm99667/JS/script.js
+++ rm99667/JS/script.js
@@ -1,37 +1,43 @@
-//Capturando o elemento h1 e adicionando em uma 
-//constante
-const h1Element = document.getElementById("Titulo")
+// //Capturando o elemento h1 e adicionando em uma 
+// //constante
+// const h1Element = document.getElementById("Titulo")
 
-//Atrelando um evento ao elemento h1
-h1Element.addEventListener("click", function () {
-    alert("Agora com super-poderes")
-  })
+// //Atrelando um evento ao elemento h1
+// h1Element.addEventListener("click", function () {
+//     alert("Agora com super-poderes")
+//   })
 
-//Agora vamos ver um array de frutas
-var Frutas = ["maça", "banana", "laranja", "melancia"]
+// //Agora vamos ver um array de frutas
+// var Frutas = ["maça", "banana", "laranja", "melancia"]
 
-console.log("MINHAS FRUTAS : " + Frutas)
-console.log("UM ITEM : " + Frutas[0])
+// console.log("MINHAS FRUTAS : " + Frutas)
+// console.log("UM ITEM : " + Frutas[0])
  
-//Adicionar um item ao final so array 
-Frutas.push("uva")
-console.log("MINHAS FRUTAS : " + Frutas)
-console.table(Frutas)
+// //Adicionar um item ao final so array 
+// Frutas.push("uva")
+// console.log("MINHAS FRUTAS : " + Frutas)
+// console.table(Frutas)
 
-for (let A = 0; A < Frutas.length; A++) {
-    const element = Frutas[A];
-    console.log("FRUTA SELECIONADA " + element)
-}
+// for (let A = 0; A < Frutas.length; A++) {
+//     const element = Frutas[A];
+//     console.log("FRUTA SELECIONADA " + element)
+// }
 
-Frutas.forEach(Fruta => {
-    console.log("Tomi essas frutas pra ti " + Fruta)
-});
+// Frutas.forEach(Fruta => {
+//     console.log("Tomi essas frutas pra ti " + Fruta)
+// });
 
 
-const listadeelementoshtml = document.getElementsByTagName("li")
-const listadeelementoshtmlarray = [...listadeelementoshtml]
+// const listadeelementoshtml = document.getElementsByTagName("li")
+// const listadeelementoshtmlarray = [...listadeelementoshtml]
 
-listadeelementoshtmlarray.forEach(elementoli =>{
-    console.log(elementoli.textContent)
+// listadeelementoshtmlarray.forEach(elementoli =>{
+//     console.log(elementoli.textContent)
 
+// })
+
+const el = [...document.getElementsByTagName("li")]
+
+el.map((e,i)=>{
+    e.innerText = "tomara que deu"
 })
